aabf760673eb3da5ed6d8c55f6d5cb48d37075c8
Brossault Romain <153521326+romainbrossault@users.noreply.github.com>
2024-12-16 11:14:23 +0100

    logo changed

--- public/scripts/intro.js
+++ public/scripts/intro.js
@@ -1,7 +1,19 @@
 window.addEventListener('load', function() {
     const intro = document.getElementById('intro');
+    const introImage = document.getElementById('intro-image');
+    const introLeft = document.createElement('div');
+    const introRight = document.createElement('div');
+
+    introLeft.id = 'intro-left';
+    introRight.id = 'intro-right';
+
+    intro.appendChild(introLeft);
+    intro.appendChild(introRight);
+
     setTimeout(() => {
-        intro.style.opacity = '0';
+        introImage.style.animation = 'slideOutLeft 1s ease-in-out forwards';
+        introLeft.style.animation = 'openBackgroundLeft 1s ease-in-out forwards';
+        introRight.style.animation = 'openBackgroundRight 1s ease-in-out forwards';
         setTimeout(() => {
             intro.style.display = 'none';
         }, 1000);
